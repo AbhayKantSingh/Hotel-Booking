6ee411342f32fcc7d7156487d3ee2d35d791ae4b
AbhayKantSingh <abhaykant1122singh@gmail.com>
2026-07-17 19:32:06 +0530

    Responsive

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const numTag = document.querySelector(".options ul li span");
 const listElement = document.querySelector(".options ul li");
+const hamburger  = document.getElementById("ham")
+const options = document.querySelector(".options")
+const navDown =document.querySelector(".nav-down")
+const main = document.querySelector("main")
 
 listElement.addEventListener("mouseenter",()=>{
     numTag.classList.remove("hide")
@@ -7,4 +11,10 @@ listElement.addEventListener("mouseenter",()=>{
 
 listElement.addEventListener("mouseleave",()=>{
     numTag.classList.add("hide")
+})
+
+hamburger.addEventListener("click",()=>{
+    options.classList.toggle("hideOptions")
+    navDown.classList.toggle("hideNav-down")
+    main.classList.toggle("pos")
 })
